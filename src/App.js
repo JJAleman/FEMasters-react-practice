7e4f2803aece347a55694cd15c4d5b2d28d35cc0
Juan Aleman <juan.j.aleman@gmail.com>
2020-03-26 15:55:08 -0500

    JSX portion completed

--- src/App.js
+++ src/App.js
@@ -3,24 +3,33 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Kiana",
-      animal: "Dog",
-      breed: "Weimaraner"
-    }),
-    React.createElement(Pet, {
-      name: "Kaiser",
-      animal: "Dog",
-      breed: "Chihuahua"
-    }),
-    React.createElement(Pet, {
-      name: "Chispita",
-      animal: "Dog",
-      breed: "Chihuahua"
-    })
-  ]);
+  // return React.createElement("div", {}, [
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(Pet, {
+  //     name: "Kiana",
+  //     animal: "Dog",
+  //     breed: "Weimaraner"
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Kaiser",
+  //     animal: "Dog",
+  //     breed: "Chihuahua"
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Chispita",
+  //     animal: "Dog",
+  //     breed: "Chihuahua"
+  //   })
+  // ]);
+
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Kiana" animal="Dog" breed="Weimaraner" />
+      <Pet name="Kaiser" animal="Dog" breed="Chihuahua" />
+      <Pet name="Chispita" animal="Dog" breed="Chihuahua" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
