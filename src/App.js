e10f65fc96802e34fcecaa9149b632a5e814572b
Juan Aleman <juan.j.aleman@gmail.com>
2020-03-26 17:31:40 -0500

    hooks

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
 import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
+import SearchParams from './SearchParams.js';
 
 const App = () => {
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, "Adopt Me!"),
-  //   React.createElement(Pet, {
-  //     name: "Kiana",
-  //     animal: "Dog",
-  //     breed: "Weimaraner"
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Kaiser",
-  //     animal: "Dog",
-  //     breed: "Chihuahua"
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Chispita",
-  //     animal: "Dog",
-  //     breed: "Chihuahua"
-  //   })
-  // ]);
 
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Kiana" animal="Dog" breed="Weimaraner" />
+      <SearchParams />
+      {/* <Pet name="Kiana" animal="Dog" breed="Weimaraner" />
       <Pet name="Kaiser" animal="Dog" breed="Chihuahua" />
-      <Pet name="Chispita" animal="Dog" breed="Chihuahua" />
+      <Pet name="Chispita" animal="Dog" breed="Chihuahua" /> */}
     </div>
   );
 };
