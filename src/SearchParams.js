6e0a371edb973778d98d93cf3c97aca4913d8f9c
Juan Aleman <juan.j.aleman@gmail.com>
2020-04-14 23:15:02 -0500

    async await was added

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,18 +1,43 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropDown from "./useDropDown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropDown] = useDropDown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropDown] = useDropDown("Breed", "", breeds);
+  const [breed, BreedDropDown, setBreed] = useDropDown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
 
-  
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    })
+
+    setPets(animals || []);
+  }
+
+  useEffect(() => {
+     setBreeds([]);
+     setBreed("");
+
+     pet.breeds(animal)
+     .then(({ breeds }) => {
+         const breedStirngs = breeds.map(({ name }) => name);
+         setBreeds(breedStirngs);
+     }, console.error);
+  }, [animal, setBreed, setBreeds]);
 
   return (
     <div className="search-params">
-      <form>
+      <form onSubmit = {
+        (e) => {
+          e.preventDefault();
+          requestPets
+        }
+      }>
         <lable htmlFor="location">
           Location
           <input
