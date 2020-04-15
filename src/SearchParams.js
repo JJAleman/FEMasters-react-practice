e4749f8acf2ddbc099bac908e87ff88dff4774c6
Juan Aleman <juan.j.aleman@gmail.com>
2020-04-14 23:39:53 -0500

    added results

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
+import Results from "./Results";
 import useDropDown from "./useDropDown";
 
 const SearchParams = () => {
@@ -13,44 +14,44 @@ const SearchParams = () => {
     const { animals } = await pet.animals({
       location,
       breed,
-      type: animal
-    })
+      type: animal,
+    });
 
     setPets(animals || []);
   }
 
   useEffect(() => {
-     setBreeds([]);
-     setBreed("");
+    setBreeds([]);
+    setBreed("");
 
-     pet.breeds(animal)
-     .then(({ breeds }) => {
-         const breedStirngs = breeds.map(({ name }) => name);
-         setBreeds(breedStirngs);
-     }, console.error);
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStirngs = breeds.map(({ name }) => name);
+      setBreeds(breedStirngs);
+    }, console.error);
   }, [animal, setBreed, setBreeds]);
 
   return (
     <div className="search-params">
-      <form onSubmit = {
-        (e) => {
+      <form
+        onSubmit={(e) => {
           e.preventDefault();
-          requestPets
-        }
-      }>
+          requestPets;
+        }}
+      >
         <lable htmlFor="location">
           Location
           <input
             id="location"
             value={location}
             placeholder="Location"
-            onChange={e => setLocation(e.target.value)}
+            onChange={(e) => setLocation(e.target.value)}
           />
         </lable>
         <AnimalDropDown />
         <BreedDropDown />
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
